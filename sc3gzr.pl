d61c75075c9d7205760e514c131e5e67982382cc
phxom <phxom@matterwave.net>
2008-11-03 01:51:11 +0000

    Sound integration. New subroutines for OSC handling as well as modificaiton in the main while() loop

--- sc3gzr.pl
+++ sc3gzr.pl
@@ -16,16 +16,21 @@
 use strict; use warnings;
 use WordNet::QueryData; 
 use Linux::Joystick;
-
+use Net::OpenSoundControl::Client;
 
 my $js = Linux::Joystick->new(
-	threshold => 2000,
+	threshold => 32000,
 	nonblocking => 1
 );
 
 print STDERR 'Loading WordNet::QueryData... ';
 my $wn = WordNet::QueryData->new;
 print "Ready.\n";
+
+my $osc_client = Net::OpenSoundControl::Client->new(
+	Host => '192.168.2.5',
+	Port => 57120 # Default sclang port (linux) (talking to OSCResponderNodes)
+) or die "Could not start client: $@\n";
 
 #### Pattern data for stick directions ####
 
@@ -322,7 +327,99 @@ sub get_rand_word_from_wn_results {
 	$word =~ s/_/ /;
 	return $1; 
 }
-	
+
+
+# OSC Subroutines
+
+sub trigger_chmod_synth {
+	# This synth is triggered when pushing buttons to change character modes
+	# \chmod synth kills itself when done (doneAction:2 on amplitude envelope)
+	my $note = shift;
+	$osc_client->send(["/chmod", "i", $note]);
+}
+
+sub anmod_right {
+	$osc_client->send(["/anmod_right"]);
+}
+
+sub anmod_left {
+	$osc_client->send(["/anmod_left"]);
+}
+
+sub anmod_mute {
+	$osc_client->send(["/anmod_mute"]);
+}
+
+sub get_stmod_formfreq {
+        my $formfreq;
+        if(!$wn_mode){ $formfreq = 100 }
+        elsif($wn_mode eq 'also'){ $formfreq = 200 }
+        elsif($wn_mode eq 'syns'){ $formfreq = 300 }
+        elsif($wn_mode eq 'hype'){ $formfreq = 400 }
+        elsif($wn_mode eq 'mero'){ $formfreq = 500 }
+        elsif($wn_mode eq 'holo'){ $formfreq = 600 }
+	else{ return 100 };
+        return $formfreq;
+}
+
+sub get_stmod_bwfreq {
+        my $bwfreq;
+        if(!$wn_mode){ $bwfreq = 100 }
+        elsif($wn_mode eq 'also'){ $bwfreq = 250 }
+        elsif($wn_mode eq 'syns'){ $bwfreq = 400 }
+        elsif($wn_mode eq 'hype'){ $bwfreq = 550 }
+        elsif($wn_mode eq 'mero'){ $bwfreq = 700 }
+        elsif($wn_mode eq 'holo'){ $bwfreq = 850 }
+	else{ return 100; }
+        return $bwfreq;
+}
+
+sub stmod_on {
+	my $note = shift;
+	$osc_client->send(["/stmod_on", "i", $note, "i", get_stmod_formfreq, "i", get_stmod_bwfreq]);
+}
+
+# Convert character chunk to note
+sub char_chunk2note {
+	my $char_chunk = shift;
+	my $char;
+	if(length($char_chunk) == 2){
+		$char = substr($char_chunk, 1, 1);
+	}else{
+		$char = $char_chunk;
+	}
+
+	my $note_num;
+	if($char eq 'a'){ $note_num = 41 } # F
+	elsif($char eq 'b'){ $note_num = 42 } # F#
+	elsif($char eq 'c'){ $note_num = 44 } # G#
+	elsif($char eq 'd'){ $note_num = 46 } # A#
+	elsif($char eq 'e'){ $note_num = 48 } # C
+	elsif($char eq 'f'){ $note_num = 49 } # C#
+	elsif($char eq 'g'){ $note_num = 51 } # D#
+        elsif($char eq 'h'){ $note_num = 53 } # F
+        elsif($char eq 'i'){ $note_num = 54 } # F#
+        elsif($char eq 'j'){ $note_num = 56 } # G#
+        elsif($char eq 'k'){ $note_num = 58 } # A#
+        elsif($char eq 'l'){ $note_num = 60 } # C
+        elsif($char eq 'm'){ $note_num = 61 } # C#
+        elsif($char eq 'n'){ $note_num = 63 } # D#
+        elsif($char eq 'o'){ $note_num = 65 } # F
+        elsif($char eq 'p'){ $note_num = 66 } # F#
+        elsif($char eq 'q'){ $note_num = 68 } # G#
+        elsif($char eq 'r'){ $note_num = 70 } # A#
+        elsif($char eq 's'){ $note_num = 72 } # C
+        elsif($char eq 't'){ $note_num = 73 } # C#
+        elsif($char eq 'u'){ $note_num = 75 } # D#
+        elsif($char eq 'v'){ $note_num = 77 } # F
+        elsif($char eq 'w'){ $note_num = 78 } # F#
+        elsif($char eq 'x'){ $note_num = 80 } # G#
+        elsif($char eq 'y'){ $note_num = 82 } # A#
+        elsif($char eq 'z'){ $note_num = 84 } # C
+
+	return $note_num;
+}
+ 	
 #### END: SUBROUTINES ####
 
 
@@ -345,6 +442,7 @@ my @lines = file2lines('text/On_the_Study_of_Words-Richard_C_Trench.txt', 80);
 my $line_ndx = 0;
 my $char_chunk = '';
 while(1){
+#while(my $e = $js->nextEvent){
 	my $e = $js->nextEvent;
 	if($e){
 		# Set $mode based on button press 
@@ -355,17 +453,27 @@ while(1){
 			# handle mode switching
 			if($e->buttonDown && $e->button != 6 && $e->button != 7){
 				$mode = $e->button;
-				#print "Mode: $mode\n"; 
+				#print "Mode: $mode\n";
+
+				# Trigger \chmod SC synth with notes based on mode number
+				if($mode == 0){ trigger_chmod_synth(54) }
+				elsif($mode == 1){ trigger_chmod_synth(56) }
+				elsif($mode == 2){ trigger_chmod_synth(60) }
+				elsif($mode == 3){ trigger_chmod_synth(63) }
+				elsif($mode == 4){ trigger_chmod_synth(66) }
+				elsif($mode == 5){ trigger_chmod_synth(70) } 
 			}
 	
 			# handle anchoring
 			if($e->buttonDown && ($e->button == 6 || $e->button == 7)){
 				if($e->buttonDown(6)){ # anchor pattern to beginning
 					$anchor = 'b';
+					anmod_left;
 					#print "Anchoring to beginning\n";
 				}
 				if($e->buttonDown(7)){ # anchor pattern to end
 					$anchor = 'e';
+					anmod_right;
 					#print "Anchoring to end\n";
 				}
 			}
@@ -373,6 +481,7 @@ while(1){
 				if($e->buttonUp(6) || $e->buttonUp(7)){
 					# Turn off anchoring if L or R was released
 					$anchor = 'n';
+					anmod_mute;
 					#print "Anchoring off\n";
 				}
 			}
@@ -417,8 +526,9 @@ while(1){
 
                			my $stick0_direction = get_stick_direction(0);
                 		if($stick0_direction){
-                        		$char_chunk = $pattern_data[$mode]->{0}->{$stick0_direction}
-                		}
+                        		$char_chunk = $pattern_data[$mode]->{0}->{$stick0_direction};
+                			stmod_on(char_chunk2note($char_chunk));
+				}
 			}
 
 			# ANALOG STICK 0 (LEFT)
@@ -454,7 +564,8 @@ while(1){
 
 				my $stick1_direction = get_stick_direction(1);
 				if($stick1_direction){
-					$char_chunk = $pattern_data[$mode]->{1}->{$stick1_direction}
+					$char_chunk = $pattern_data[$mode]->{1}->{$stick1_direction};
+                                        stmod_on(char_chunk2note($char_chunk));
 				}
 			}
 
@@ -493,7 +604,8 @@ while(1){
 
 				my $stick2_direction = get_stick_direction(2);
 				if($stick2_direction){
-					$char_chunk = $pattern_data[$mode]->{2}->{$stick2_direction}
+					$char_chunk = $pattern_data[$mode]->{2}->{$stick2_direction};
+                                        stmod_on(char_chunk2note($char_chunk));
 				}
 			}
 
@@ -536,21 +648,26 @@ while(1){
 		}
 		
 		my @line = split /\s+/, $lines[$line_ndx];
+		
 		foreach(@line){
-			if($_ =~ /$pattern/){ 
-				if(!$wn_mode){
-					print "$_ ";
-				}else{
-					my @wn_results = $wn->querySense("$_", "$wn_mode");
-					#my @wn_results = $wn->querySense("run", "syns");
-					if(@wn_results){
-						my $wn_results = join(',',@wn_results);
-						print get_rand_word_from_wn_results($wn_results);
-					}else{
+			if($pattern){  # this conditional wrapper is a bug work-around -- sometimes $pattern doesn't get assigned
+				if($_ =~ /$pattern/){ 
+					if(!$wn_mode){
 						print "$_ ";
-					}
-				} 
-			}else{ 
+					}else{
+						my @wn_results = $wn->querySense("$_", "$wn_mode");
+						#my @wn_results = $wn->querySense("run", "syns");
+						if(@wn_results){
+							my $wn_results = join(',',@wn_results);
+							print get_rand_word_from_wn_results($wn_results);
+						}else{
+							print "$_ ";
+						}
+					} 
+				}else{ 
+					print ' ' x (length $_)
+				}
+			}else{
 				print ' ' x (length $_)
 			}
 		}
@@ -564,5 +681,6 @@ while(1){
 		}
 					
 	} # end: event detector
-	
+
+	#sleep(1);	
 }	
